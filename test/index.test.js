09c8c9012eb6a25754346c5ceccee05a329aa7d3
Chris Shum <chris.shum@mongodb.com>
2016-07-12 13:35:35 -0400

    testing added

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,47 @@
-var jiraApiWrapper = require('../');
-var assert = require('assert');
+/* eslint no-unused-expressions:0 */
 
-describe('jira-api-wrapper', function(){
-  it('should work', function(){
-    assert(jiraApiWrapper);
+var jiraApiWrapper = require('../lib/index.js');
+var request = require('request');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var testVars = require('./testVars.js');
+
+describe('jira api wrapper testing', function() {
+  before(function(done) {
+    var stub = sinon.stub(request, 'get');
+    stub.withArgs(testVars.issuesOptions).yields(null, { statusCode: '200' }, testVars.issuesOutput);
+    done();
+  });
+
+  after(function(done) {
+    request.get.restore();
+    done();
+  });
+
+  it('api should exist', function() {
+    expect(jiraApiWrapper).to.exist;
+  });
+
+  it('getIssuesJQL with no jql should return error', function(done) {
+    jiraApiWrapper.getIssuesJQL(null, function(err, res) {
+      expect(err).to.equal('NO JQL PROVIDED');
+      expect(res).to.be.undefined;
+      done();
+    });
+  });
+
+  it('getIssuesJQL with jql should return a stream', function(done) {
+    jiraApiWrapper.getIssuesJQL('', function(err, res) {
+      if (err) return done(err);
+      expect(res).to.be.an('object');
+      expect(res.__HighlandStream__).to.be.true;
+      var dataCounter = 0;
+      res.on('data', function(data) {
+        data.to.equal(testVars.issuesOutput.issues[dataCounter]);
+        dataCounter++;
+      });
+      done();
+    });
   });
 });
